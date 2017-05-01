4261019d0579ac873fb172a88a3c1cd84f53e257
Ian Kurzrock <kurzroi@sunyit.edu>
2017-04-30 22:04:41 -0400

    Updated drift3.asm

--- drift3.asm
+++ drift3.asm
@@ -1,6 +1,6 @@
 # peripheral constants
 
-at robots
+.const P_THROTTLE 0x4000
 .const P_DIST_LEFT 0x4001 # dist in pixels. <0 if off road
 .const P_DIST_RIGHT 0x4002 # dist in pixels. <0 if off road
 .const P_DIST_FRONT 0x4003 # dist in pixels. <0 if off road
@@ -11,16 +11,181 @@ at robots
 	movi r100 = 50
 	stor [P_THROTTLE] = r100
 	load r100 = [P_THROTTLE]
-	sub r100, 5
-	sub r100, 5
-	sub r100, 5
-	sub r100, 5
-	sub r100, 5
-	sub r100, 5
-	sub r100, 5
-	sub r100, 5
-	sub r100, 5
-	sub r100, 5
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	movi r100 = 45
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	movi r100 = 40
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	movi r100 = 35
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	movi r100 = 38
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	movi r100 = 39
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	NOP
+	movi r100 = 25
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	movi r100 = 20
+	stor [P_THROTTLE] = r100
+	load r100 = [P_THROTTLE]
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	movi r104 = 90
+	stor [P_STEERING] = r104
+	stor [P_STEERING] = r104
+	stor [P_STEERING] = r104
+	stor [P_STEERING] = r104
+	stor [P_STEERING] = r104
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	movi r104 = -45
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	stor [P_STEERING] = r104
+	stor [P_STEERING] = r104
+	stor [P_STEERING] = r104
+	stor [P_STEERING] = r104
+	stor [P_STEERING] = r104
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	movi r104 = 0
+	stor [P_STEERING] = r104
+	load r101 = [P_DIST_LEFT]
+	load r102 = [P_DIST_RIGHT]
+	load r103 = [P_DIST_FRONT]
 
 !inf
 	bra [!inf]
